cf4f82f646f1bdeab8d2872a7ef29bbdcf451f1e
Gajanan Paralkar <gajananp@olivecrypto.com>
2025-08-02 14:31:22 +0530

    added new project

---
+++ src/test/java/com/dev/redisspring/RedisWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.redisspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
